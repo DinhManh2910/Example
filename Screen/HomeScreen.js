311d6d50b7b0343514c87744d18e8238b04169ba
nguyendinhmanh <manh01643141920@gmail.com>
2020-03-12 11:08:48 +0700

    create HomeScreen

---
+++ Screen/HomeScreen.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class HomeScreen extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <View style={{flex:1, justifyContent:"center"}}>
+                <Text>Học code dạo</Text>
+            </View>
+        );
+    }
+}
+
+export default HomeScreen;
